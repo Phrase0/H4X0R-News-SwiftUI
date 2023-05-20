0e5ed0969ee71e40aaa6e09cfee76c494920f8e8
Peiyun <b612starnight@gmail.com>
2023-05-20 23:53:49 +0800

    Initial Commit

---
+++ H4X0R News-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News-SwiftUI
+//
+//  Created by Peiyun on 2023/5/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4X0R News-SwiftUI/H4X0R_News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_News_SwiftUIApp.swift
+//  H4X0R News-SwiftUI
+//
+//  Created by Peiyun on 2023/5/20.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
